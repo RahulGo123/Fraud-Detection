11a4c54343568c22471f15a55ba3b96c3bd92477
Rahul <goyal.rahul7700@gmail.com>
2025-09-16 17:20:27 +0530

    Data Validation Completed

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from frauddetection.components.data_ingestion import DataIngestion
+from frauddetection.components.data_validation import DataValidation
 from frauddetection.exception.exception import FraudDetectionException
 from frauddetection.logging.logger import logging
-from frauddetection.entity.config_entity import DataIngestionConfig
+from frauddetection.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from frauddetection.entity.config_entity import TrainingPipelineConfig
 
 
@@ -16,6 +17,13 @@ if __name__=="__main__":
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
         logging.info("Data Ingestion Completed")
         print(data_ingestion_artifact)
+        
+        data_validation_config = DataValidationConfig(training_pipeline_config)
+        data_validation = DataValidation(data_ingestion_artifact, data_validation_config)
+        logging.info("Initiate the data validation")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("Data Validation Completed")
+        print(data_validation_artifact)
 
     except Exception as e:
         raise FraudDetectionException(e, sys)
